a72723b784c869c059b0614af13f889a4f89aa27
julischa <julianeschawert@gmail.com>
2025-03-02 22:23:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(otf|ttf)$/i,
+      type: "asset/resource",
+      generator: {
+        filename: "fonts/[name][ext]",
+      },
+    });
+    return config;
+  },
+};
